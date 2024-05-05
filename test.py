0dba7e56c5a43fa000c4ae17d0861d1f6228821d
Guodafeng-tl <2297743001@qq.com>
2024-05-05 15:37:34 +0800

    test

---
+++ test.py
@@ -0,0 +1,27 @@
+print("dafeng-test")
+
+# 单行注释
+print("dafeng")
+
+"""
+    多行注释
+    dafeng
+"""
+print("dafeng-test")
+
+money = 50
+print("原始====", money)
+money = money-20
+print("减去二十", money)
+
+print(type(88))
+print(type("dafeng"))
+print(type(3.14))
+
+
+name_str = str(88)
+print(type(name_str),name_str)
+
+#整数转换成float  精度补零
+name_float =  float(200)
+print(type(name_float),name_float)
