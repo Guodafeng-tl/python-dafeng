f9df852f3c70ce88cb77d839d413e8e70d36858c
Guodafeng-tl <2297743001@qq.com>
2024-05-05 16:43:44 +0800

    循环test

---
+++ 循环test.py
@@ -0,0 +1,20 @@
+i = 0
+sumValue = 0
+while i < 100:
+    print("kk,I love you %d" % i)
+    i += 1
+    sumValue = sumValue + i
+
+print("sum=== %d " % sumValue)
+
+# 不换行 end=''
+
+print("hello")
+print("world")
+
+print("hello", end='')
+print("world", end='')
+
+print("9999===")
+
+print("hello\tworld")
