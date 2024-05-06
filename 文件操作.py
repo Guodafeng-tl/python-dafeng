5f15f09311a7c22d84ddae190847a0a9a9beeeb5
Guodafeng-tl <2297743001@qq.com>
2024-05-06 20:57:25 +0800

    文件写入

--- 文件操作.py
+++ 文件操作.py
@@ -15,4 +15,16 @@ with open("D:/python/python-learn/dafeng-learn/test.txt", "r", encoding="UTF-8")
     for x in test:
         print(f"============{x}")
 
+print("wwwwwwwwwwwwwwwwwwwww=")
+
+
+#w模式  文件不存在 创建 、文件存在  覆盖文件内容
+f = open("D:/python/python-learn/dafeng-learn/dafeng.txt", "w", encoding="UTF-8")
+f.write("I LOVE PYTHON")
+f.flush()
+f.close()
+print("sssssssssssssssssss")
+
+#a模式文件不存在 创建文件、文件存在 追加文件内容
+
 
